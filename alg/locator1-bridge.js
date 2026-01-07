668fd59e37efbda3d99f6415126301b71c9fc6a6
guojia4112 <jiaguo@ust.hk>
2026-01-07 17:59:44 +0800

    Delete alg directory

--- alg/locator1-bridge.js
+++
@@ -1,5 +0,0 @@
-
-// alg/locator1-bridge.js (ESM)
-// 暴露：window.Locator1 { initGridFromCSV, isGridReady, estimateContinuousIsoFromXY, estimateFromXY }
-import { initGridFromCSV, isGridReady, estimateContinuousIsoFromXY, estimateFromXY } from './locator1.js';
-window.Locator1 = { initGridFromCSV, isGridReady, estimateContinuousIsoFromXY, estimateFromXY };
--- alg/locator2-bridge.js
+++
@@ -1,4 +0,0 @@
-
-// alg/locator2-bridge.js (ESM)
-import { initGridFromCSV, isGridReady, interpolateXYFromVOCNOX } from './locator2.js';
-window.Locator2 = { initGridFromCSV, isGridReady, interpolateXYFromVOCNOX };
